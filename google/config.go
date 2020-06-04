5f6999ef64ce561591d1f3d270a3356decd79780
Modular Magician <magic-modules@google.com>
2020-06-04 00:57:27 +0000

    Add Network Management ConnectivityTest resource (#3601)
    * add filename override
    
    * initial network management resource
    
    * description
    
    * comments
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/config.go
+++ google/config.go
@@ -105,6 +105,7 @@ type Config struct {
 	LoggingBasePath              string
 	MLEngineBasePath             string
 	MonitoringBasePath           string
+	NetworkManagementBasePath    string
 	OSLoginBasePath              string
 	PubsubBasePath               string
 	RedisBasePath                string
@@ -246,6 +247,7 @@ var KMSDefaultBasePath = "https://cloudkms.googleapis.com/v1/"
 var LoggingDefaultBasePath = "https://logging.googleapis.com/v2/"
 var MLEngineDefaultBasePath = "https://ml.googleapis.com/v1/"
 var MonitoringDefaultBasePath = "https://monitoring.googleapis.com/"
+var NetworkManagementDefaultBasePath = "https://networkmanagement.googleapis.com/v1/"
 var OSLoginDefaultBasePath = "https://oslogin.googleapis.com/v1/"
 var PubsubDefaultBasePath = "https://pubsub.googleapis.com/v1/"
 var RedisDefaultBasePath = "https://redis.googleapis.com/v1/"
@@ -762,6 +764,7 @@ func ConfigureBasePaths(c *Config) {
 	c.LoggingBasePath = LoggingDefaultBasePath
 	c.MLEngineBasePath = MLEngineDefaultBasePath
 	c.MonitoringBasePath = MonitoringDefaultBasePath
+	c.NetworkManagementBasePath = NetworkManagementDefaultBasePath
 	c.OSLoginBasePath = OSLoginDefaultBasePath
 	c.PubsubBasePath = PubsubDefaultBasePath
 	c.RedisBasePath = RedisDefaultBasePath
