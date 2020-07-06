8e5b1d2d05f3d997761abb497980dfdf78218302
Modular Magician <magic-modules@google.com>
2020-07-06 19:54:05 +0000

    add os config patch deployment resource (#3721)
    * add os config patch deployment
    
    * add validation fix encoder
    
    * remove commented out code
    
    * add goo to patchConfig
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/config.go
+++ google/config.go
@@ -106,6 +106,7 @@ type Config struct {
 	MLEngineBasePath             string
 	MonitoringBasePath           string
 	NetworkManagementBasePath    string
+	OSConfigBasePath             string
 	OSLoginBasePath              string
 	PubsubBasePath               string
 	RedisBasePath                string
@@ -248,6 +249,7 @@ var LoggingDefaultBasePath = "https://logging.googleapis.com/v2/"
 var MLEngineDefaultBasePath = "https://ml.googleapis.com/v1/"
 var MonitoringDefaultBasePath = "https://monitoring.googleapis.com/"
 var NetworkManagementDefaultBasePath = "https://networkmanagement.googleapis.com/v1/"
+var OSConfigDefaultBasePath = "https://osconfig.googleapis.com/v1/"
 var OSLoginDefaultBasePath = "https://oslogin.googleapis.com/v1/"
 var PubsubDefaultBasePath = "https://pubsub.googleapis.com/v1/"
 var RedisDefaultBasePath = "https://redis.googleapis.com/v1/"
@@ -766,6 +768,7 @@ func ConfigureBasePaths(c *Config) {
 	c.MLEngineBasePath = MLEngineDefaultBasePath
 	c.MonitoringBasePath = MonitoringDefaultBasePath
 	c.NetworkManagementBasePath = NetworkManagementDefaultBasePath
+	c.OSConfigBasePath = OSConfigDefaultBasePath
 	c.OSLoginBasePath = OSLoginDefaultBasePath
 	c.PubsubBasePath = PubsubDefaultBasePath
 	c.RedisBasePath = RedisDefaultBasePath
