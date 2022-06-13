61e8df929376f7f6c827bdb914acb7c83cb54776
Stephen Lewis (Burrows) <stephenrlewis@google.com>
2022-06-13 08:12:18 -0700

    Fleshed out TFPlanToCAI implementation (#801)
    For now, this is mostly the same as the internals of ReadPlannedAssets with two major differences:
    
    1. Take an array of bytes rather than a filename. This means the library doesn't need to deal with file I/O.
    2. Logic from newConverter is inlined. This is because newConverter is not exported (and is too similar to NewConverter in name) and it doesn't actually provide much utility as a separate function.

--- conversion/tfplan_to_cai.go
+++ conversion/tfplan_to_cai.go
@@ -1,31 +1,90 @@
 package conversion
 
 import (
-	"go.uber.org/zap"
+	"context"
+	"fmt"
 	"net/http"
+
+	"github.com/GoogleCloudPlatform/terraform-validator/ancestrymanager"
+	"github.com/GoogleCloudPlatform/terraform-validator/converters/google"
+	resources "github.com/GoogleCloudPlatform/terraform-validator/converters/google/resources"
+	"github.com/GoogleCloudPlatform/terraform-validator/tfplan"
+	"go.uber.org/zap"
+	"google.golang.org/api/cloudresourcemanager/v3"
 )
 
 // Single struct for options so that adding new options does not require
 // updating function signatures all along the pipe
 type TFPlanToCAIOptions struct {
+	Context          *context.Context
 	ConvertUnchanged bool
 	ErrorLogger      *zap.Logger
 	Offline          bool
 	DefaultProject   string
-	DefaultRegion    string
-	DefaultZone      string
+	// TODO: 
+	// DefaultRegion    string
+	// DefaultZone      string
 	// UserAgent for all requests (if online)
 	UserAgent string
 	// HTTPClient for all requests (if online)
 	HTTPClient *http.Client
 	// Map hierarchy resource (like projects/<number> or folders/<number>)
-	// to a cached ancestry
+	// to an ancestry path (like organizations/123/folders/456/projects/789)
 	AncestryCache map[string]string
 }
 
-func TFPlanToCAI(ctx context.Context, jsonPlan []byte, options *TFPlanToCAIOptions) ([]Asset, error) {
+type TFPlanToCAIFunc func(jsonPlan []byte, o *TFPlanToCAIOptions) ([]Asset, error)
+
+func TFPlanToCAI(jsonPlan []byte, o *TFPlanToCAIOptions) ([]Asset, error) {
 	// Creates ancestry manager and converter internally; they are
 	// implementation details private to this package.
-	var assets []Asset
-	return assets, nil
+	var ctx context.Context
+	if o.Context == nil {
+		ctx = context.Background()
+	} else {
+		ctx = *o.Context
+	}
+
+	errorLogger := o.ErrorLogger
+	if errorLogger == nil {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			return nil, fmt.Errorf("building logger: %w", err)
+		}
+		errorLogger = logger
+	}
+
+	// contents of newConverter
+	// Set up config and ancestry manager using the same http client and user agent
+	cfg, err := resources.NewConfig(ctx, o.DefaultProject, o.Offline, o.UserAgent, o.HTTPClient)
+	if err != nil {
+		return nil, fmt.Errorf("building config: %w", err)
+	}
+
+	var resourceManager *cloudresourcemanager.Service
+	if !o.Offline {
+		// This will create a resource manager client that uses the same
+		// underlying client and the same user agent that the cfg uses to
+		// ensure consistent API access.
+		resourceManager = cfg.NewResourceManagerV3Client(cfg.UserAgent())
+	}
+	ancestryManager, err := ancestrymanager.New(resourceManager, o.AncestryCache, errorLogger)
+	if err != nil {
+		return nil, fmt.Errorf("building ancestry manager: %w", err)
+	}
+
+	converter := google.NewConverter(cfg, ancestryManager, o.Offline, o.ConvertUnchanged, errorLogger)
+
+	// ReadResourceChanges
+	changes, err := tfplan.ReadResourceChanges(jsonPlan)
+	if err != nil {
+		return nil, err
+	}
+
+	err = converter.AddResourceChanges(changes)
+	if err != nil {
+		return nil, err
+	}
+
+	return converter.Assets(), nil
 }
