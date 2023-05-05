7271241d14ce71304bd2ffbdfdf80cc6bf1b1e2d
Modular Magician <magic-modules@google.com>
2023-05-05 00:13:53 +0000

    Move Type Structs to the packages (#7876)
    * Move types to the packages
    
    * Move mutexkv.go to the transport package
    
    * Replace functions for DCL
    
    * Define a global variable TimeoutErr
    
    * Move the function LockedCall to the transport package
    
    * Deprecate var mutexKV
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/accessapproval_folder_settings.go
+++ tfplan2cai/converters/google/resources/accessapproval_folder_settings.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
+	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/tfplan2cai/converters/google/resources/tpgresource"
 	transport_tpg "github.com/GoogleCloudPlatform/terraform-google-conversion/v2/tfplan2cai/converters/google/resources/transport"
 )
 
