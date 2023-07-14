1096f24b1cf980fc126f2a6da8e806d76f28f36d
Iris Chen <ciris@google.com>
2023-07-14 12:28:29 -0400

    fix conflict

--- tfplan2cai/converters/google/resources/services/networksecurity/networksecurity_gateway_security_policy.go
+++ tfplan2cai/converters/google/resources/services/networksecurity/networksecurity_gateway_security_policy.go
@@ -59,15 +59,12 @@ func GetNetworkSecurityGatewaySecurityPolicyApiObject(d tpgresource.TerraformRes
 	} else if v, ok := d.GetOkExists("description"); !tpgresource.IsEmptyValue(reflect.ValueOf(descriptionProp)) && (ok || !reflect.DeepEqual(v, descriptionProp)) {
 		obj["description"] = descriptionProp
 	}
-<<<<<<< HEAD
-=======
 	tlsInspectionPolicyProp, err := expandNetworkSecurityGatewaySecurityPolicyTlsInspectionPolicy(d.Get("tls_inspection_policy"), d, config)
 	if err != nil {
 		return nil, err
 	} else if v, ok := d.GetOkExists("tls_inspection_policy"); !tpgresource.IsEmptyValue(reflect.ValueOf(tlsInspectionPolicyProp)) && (ok || !reflect.DeepEqual(v, tlsInspectionPolicyProp)) {
 		obj["tlsInspectionPolicy"] = tlsInspectionPolicyProp
 	}
->>>>>>> d85c314 (all change required to pass test)
 
 	return obj, nil
 }
