98595d977aafc61caf49df1705862da2be3ed954
Modular Magician <magic-modules@google.com>
2023-09-05 23:30:13 +0000

    Adding labels field to the GKEHub Scope RBAC RoleBinding resource (#8869)
    * Adding Terraform resources for Tenancy APIs in GKEHub
    
    * Segregating MembershipBinding and MembershipRBACRoleBinding to keep things simpler in the review
    
    * Fixing the docu URIs
    
    * Adding TF support for Tenancy API for Membership Binding
    
    * Adding dependent membership binding to the same commit chain
    
    * Making Scope un-updatable and replacing hard coded project number with the one from test env
    
    * Making Scope RRBAC updatable
    
    * Making Namespace immutable
    
    * Adding update test cases
    
    * Removing all memberships field from Scope since it is no longer supported
    
    * Removing all_memberships field for Scope from all test cases
    
    * Adding labels to the Scope RBAC Role Binding resource
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/gkehub2/gkehub2_scope_rbac_role_binding.go
+++ tfplan2cai/converters/google/resources/services/gkehub2/gkehub2_scope_rbac_role_binding.go
@@ -72,6 +72,12 @@ func GetGKEHub2ScopeRBACRoleBindingApiObject(d tpgresource.TerraformResourceData
 	} else if v, ok := d.GetOkExists("role"); !tpgresource.IsEmptyValue(reflect.ValueOf(roleProp)) && (ok || !reflect.DeepEqual(v, roleProp)) {
 		obj["role"] = roleProp
 	}
+	labelsProp, err := expandGKEHub2ScopeRBACRoleBindingLabels(d.Get("labels"), d, config)
+	if err != nil {
+		return nil, err
+	} else if v, ok := d.GetOkExists("labels"); !tpgresource.IsEmptyValue(reflect.ValueOf(labelsProp)) && (ok || !reflect.DeepEqual(v, labelsProp)) {
+		obj["labels"] = labelsProp
+	}
 
 	return obj, nil
 }
@@ -105,4 +111,15 @@ func expandGKEHub2ScopeRBACRoleBindingRole(v interface{}, d tpgresource.Terrafor
 
 func expandGKEHub2ScopeRBACRoleBindingRolePredefinedRole(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
 	return v, nil
+}
+
+func expandGKEHub2ScopeRBACRoleBindingLabels(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (map[string]string, error) {
+	if v == nil {
+		return map[string]string{}, nil
+	}
+	m := make(map[string]string)
+	for k, val := range v.(map[string]interface{}) {
+		m[k] = val.(string)
+	}
+	return m, nil
 }
