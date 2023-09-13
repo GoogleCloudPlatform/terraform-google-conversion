85d1518acb9d6ce4efc47907b4f00ee14472a1e0
The Magician <magic-modules@google.com>
2023-09-13 16:08:24 -0400

    Transitioned `volumes.cloud_sql_instance.instances` to SET from array (#8929) (#1427)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/cloudrunv2/cloudrunv2_service.go
+++ tfplan2cai/converters/google/resources/services/cloudrunv2/cloudrunv2_service.go
@@ -17,6 +17,8 @@ package cloudrunv2
 import (
 	"reflect"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/tfplan2cai/converters/google/resources/cai"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta/transport"
@@ -1319,6 +1321,7 @@ func expandCloudRunV2ServiceTemplateVolumesCloudSqlInstance(v interface{}, d tpg
 }
 
 func expandCloudRunV2ServiceTemplateVolumesCloudSqlInstanceInstances(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
+	v = v.(*schema.Set).List()
 	return v, nil
 }
 
