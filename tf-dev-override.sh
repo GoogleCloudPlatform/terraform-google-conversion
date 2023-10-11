3a950ec4a295d21349d44a37bf77a64d53a961b3
Iris Chen <ciris@google.com>
2023-10-11 18:42:20 -0400

    Use tf dev override in integration test

---
+++ tf-dev-override.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ex
+
+# required for go install terraform-provider-google-beta
+go mod download github.com/hashicorp/terraform-plugin-mux
+
+go install github.com/hashicorp/terraform-provider-google-beta
+
+# create terraform configuration file
+cat <<EOF > /tmp/tf-dev-override.tfrc
+provider_installation {
+  # Developer overrides will stop Terraform from downloading the listed
+  # providers their origin provider registries.
+  dev_overrides {
+      "hashicorp/google-beta" = "$GOPATH/bin"
+  }
+  # For all other providers, install them directly from their origin provider
+  # registries as normal. If you omit this, Terraform will _only_ use
+  # the dev_overrides block, and so no other providers will be available.
+  # Without this, show "Failed to query available provider packages"
+  # at terraform init
+  direct{}
+}
+EOF
+
+# if the test code detects this env is set, it will add into the cmd execute.
+export TF_CLI_CONFIG_FILE="/tmp/tf-dev-override.tfrc"
