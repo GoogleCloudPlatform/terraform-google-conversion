ef729da3ffd606b3633664016ccd8f24ee7b453d
Iris Chen <ciris@google.com>
2023-11-07 23:34:21 -0800

    test failure

--- tfplan2cai/testdata/templates/example_access_context_manager_service_perimeter.tf
+++ tfplan2cai/testdata/templates/example_access_context_manager_service_perimeter.tf
@@ -60,7 +60,7 @@ resource "google_access_context_manager_service_perimeter" "service-perimeter" {
         sources {
           access_level = "accessPolicies/987654/accessLevels/restrict_storage"
         }
-	source_restriction = "SOURCE_RESTRICTION_ENABLED"
+	      source_restriction = "SOURCE_RESTRICTION_ENABLED"
         identity_type = "ANY_USER_ACCOUNT"
       }
     }
