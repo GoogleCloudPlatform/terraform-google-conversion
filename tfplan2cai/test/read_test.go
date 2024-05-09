bd7c627ef0bd6be51e200d580b08dde035ceca9f
The Magician <magic-modules@google.com>
2024-05-09 10:39:14 -0700

    Add support for `google_kms_key_ring_import_job` to TGC (#10554) (#2240)
    [upstream:54117696fb9d980d9206ef2467bb52a9be6eb80b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/test/read_test.go
+++ tfplan2cai/test/read_test.go
@@ -83,6 +83,7 @@ func TestReadPlannedAssetsCoverage(t *testing.T) {
 		{name: "example_kms_crypto_key_iam_binding"},
 		{name: "example_kms_crypto_key_iam_member"},
 		{name: "example_kms_crypto_key_iam_policy"},
+		{name: "example_kms_import_job"},
 		{name: "example_kms_key_ring"},
 		{name: "example_kms_key_ring_iam_binding"},
 		{name: "example_kms_key_ring_iam_member"},
