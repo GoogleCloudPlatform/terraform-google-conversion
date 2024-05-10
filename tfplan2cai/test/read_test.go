ab02cb27a3289f8a6c02bb5bf796c08ecc7a4b74
The Magician <magic-modules@google.com>
2024-05-10 10:19:02 -0700

    Add Suport for `google_compute_resource_policy` to TGC (#10584) (#2248)
    [upstream:a3614a91dfe1f43568f6bd426a635352104084b7]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/test/read_test.go
+++ tfplan2cai/test/read_test.go
@@ -77,6 +77,7 @@ func TestReadPlannedAssetsCoverage(t *testing.T) {
 		{name: "example_google_cloudfunctions_function"},
 		{name: "example_google_compute_autoscaler"},
 		{name: "example_google_compute_network_endpoint_group"},
+		{name: "example_google_compute_resource_policy"},
 		{name: "example_google_compute_target_http_proxy"},
 		{name: "example_google_dataproc_autoscaling_policy"},
 		{name: "example_google_datastream_connection_profile"},
