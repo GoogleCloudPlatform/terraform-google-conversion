0848edebebacee0f63dadd33639f60f351e63f20
The Magician <magic-modules@google.com>
2024-05-20 12:05:25 -0700

    Add support for ```google_datastream_private_connection``` to TGC (#10706) (#2282)
    [upstream:bcb479447f3abca729d5bcbea6934d997dd1384e]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/test/read_test.go
+++ tfplan2cai/test/read_test.go
@@ -88,6 +88,7 @@ func TestReadPlannedAssetsCoverage(t *testing.T) {
 		{name: "example_google_compute_target_http_proxy"},
 		{name: "example_google_dataproc_autoscaling_policy"},
 		{name: "example_google_datastream_connection_profile"},
+		{name: "example_google_datastream_private_connection"},
 		{name: "example_google_datastream_stream"},
 		{name: "example_google_sql_database"},
 		{name: "example_kms_crypto_key"},
