e4a4db40096093e951457d48178b335b1f780c4e
Modular Magician <magic-modules@google.com>
2024-06-13 17:15:09 +0000

    add support for `google_app_engine_application` to TCG (#10944)
    [upstream:11fe43ce7ee36b576190a17f60129d1911b6b187]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/test/read_test.go
+++ tfplan2cai/test/read_test.go
@@ -37,6 +37,7 @@ func TestReadPlannedAssetsCoverage(t *testing.T) {
 		{name: "example_access_context_manager_service_perimeter"},
 		{name: "example_alloydb_instance"},
 		{name: "example_apikeys_key"},
+		{name: "example_app_engine_application"},
 		{name: "example_artifact_registry_repository"},
 		{name: "example_bigquery_dataset"},
 		{name: "example_bigquery_dataset_iam_binding"},
