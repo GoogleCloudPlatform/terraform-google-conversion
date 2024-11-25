d12f3b00128da215d9377d73160f5350402bd125
Zhenhua Li <zhenhuali@google.com>
2024-11-25 11:02:32 -0800

    Add initial commands in TGC v6

---
+++ v6/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/GoogleCloudPlatform/terraform-google-conversion/v6/cmd"
+)
+
+func main() {
+	// Workaround for "ERROR: logging before flag.Parse"
+	fs := flag.NewFlagSet("", flag.ContinueOnError)
+	_ = fs.Parse([]string{})
+	flag.CommandLine = fs
+
+	cmd.Execute()
+}
