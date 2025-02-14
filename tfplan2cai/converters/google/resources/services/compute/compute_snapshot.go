e8aaa8c39efa7821ca461c500064d1055db8f315
Modular Magician <magic-modules@google.com>
2025-02-14 00:45:50 +0000

    Add missing fields to compute resources [KMS and disks] (#12672)
    [upstream:d7f8e8a5a42907f8a0e072fb585c92ef9b32335a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/compute/compute_snapshot.go
+++ tfplan2cai/converters/google/resources/services/compute/compute_snapshot.go
@@ -182,6 +182,13 @@ func expandComputeSnapshotSnapshotEncryptionKey(v interface{}, d tpgresource.Ter
 		transformed["rawKey"] = transformedRawKey
 	}
 
+	transformedRsaEncryptedKey, err := expandComputeSnapshotSnapshotEncryptionKeyRsaEncryptedKey(original["rsa_encrypted_key"], d, config)
+	if err != nil {
+		return nil, err
+	} else if val := reflect.ValueOf(transformedRsaEncryptedKey); val.IsValid() && !tpgresource.IsEmptyValue(val) {
+		transformed["rsaEncryptedKey"] = transformedRsaEncryptedKey
+	}
+
 	transformedSha256, err := expandComputeSnapshotSnapshotEncryptionKeySha256(original["sha256"], d, config)
 	if err != nil {
 		return nil, err
@@ -207,6 +214,10 @@ func expandComputeSnapshotSnapshotEncryptionKey(v interface{}, d tpgresource.Ter
 }
 
 func expandComputeSnapshotSnapshotEncryptionKeyRawKey(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
+	return v, nil
+}
+
+func expandComputeSnapshotSnapshotEncryptionKeyRsaEncryptedKey(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
 	return v, nil
 }
 
