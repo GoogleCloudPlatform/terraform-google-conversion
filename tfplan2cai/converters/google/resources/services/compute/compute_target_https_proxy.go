5f2e844e8bc24d21c842446add448d656f39d3d6
Zhenhua Li <zhenhuali@google.com>
2025-02-21 10:00:18 -0800

    chore: upgrade tgc module to v6 (#3495)

--- tfplan2cai/converters/google/resources/services/compute/compute_target_https_proxy.go
+++ tfplan2cai/converters/google/resources/services/compute/compute_target_https_proxy.go
@@ -21,7 +21,7 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/GoogleCloudPlatform/terraform-google-conversion/v5/tfplan2cai/converters/google/resources/cai"
+	"github.com/GoogleCloudPlatform/terraform-google-conversion/v6/tfplan2cai/converters/google/resources/cai"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta/transport"
 )
