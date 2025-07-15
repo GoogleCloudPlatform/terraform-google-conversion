8f6ae7b6ffc65d95133ed347d95aaabbf3c32d4e
The Magician <magic-modules@google.com>
2025-07-15 14:50:44 -0700

    Add resource_manager_tags support to Subnetwork api (#14398) (#4183)
    [upstream:490a414a5b05cadaef67a1c8c792019e8d6d8bcf]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- pkg/services/compute/compute_subnetwork.go
+++ pkg/services/compute/compute_subnetwork.go
@@ -252,6 +252,29 @@ Can only be specified if VPC flow logs for this subnetwork is enabled and "metad
 					},
 				},
 			},
+			"params": {
+				Type:        schema.TypeList,
+				Optional:    true,
+				ForceNew:    true,
+				Description: `Additional params passed with the request, but not persisted as part of resource payload`,
+				MaxItems:    1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"resource_manager_tags": {
+							Type:     schema.TypeMap,
+							Optional: true,
+							ForceNew: true,
+							Description: `Resource manager tags to be bound to the subnetwork. Tag keys and values have the
+same definition as resource manager tags. Keys must be in the format tagKeys/{tag_key_id},
+and values are in the format tagValues/456. The field is ignored when empty.
+The field is immutable and causes resource replacement when mutated. This field is only
+set at create time and modifying this field after creation will trigger recreation.
+To apply tags to an existing resource, see the google_tags_tag_binding resource.`,
+							Elem: &schema.Schema{Type: schema.TypeString},
+						},
+					},
+				},
+			},
 			"private_ip_google_access": {
 				Type:     schema.TypeBool,
 				Computed: true,
