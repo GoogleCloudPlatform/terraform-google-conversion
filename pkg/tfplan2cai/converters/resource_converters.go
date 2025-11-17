7f5c20d15d3d0c26273c0f1f01525893fb9227bf
Modular Magician <magic-modules@google.com>
2025-11-17 18:18:57 +0000

    tgc-revival: add google_logging_metric (#15718)
    [upstream:c0d20fb2061823bb8e648201313f7d85076d1828]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- pkg/tfplan2cai/converters/resource_converters.go
+++ pkg/tfplan2cai/converters/resource_converters.go
@@ -36,6 +36,7 @@ import (
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/services/firebasedataconnect"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/services/iambeta"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/services/kms"
+	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/services/logging"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/services/monitoring"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/services/networksecurity"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/services/pubsub"
@@ -109,6 +110,7 @@ var ConverterMap = map[string]cai.Tfplan2caiConverter{
 	"google_iam_workload_identity_pool_provider":           iambeta.IAMBetaWorkloadIdentityPoolProviderTfplan2caiConverter(),
 	"google_kms_autokey_config":                            kms.KMSAutokeyConfigTfplan2caiConverter(),
 	"google_kms_key_handle":                                kms.KMSKeyHandleTfplan2caiConverter(),
+	"google_logging_metric":                                logging.LoggingMetricTfplan2caiConverter(),
 	"google_monitoring_notification_channel":               monitoring.MonitoringNotificationChannelTfplan2caiConverter(),
 	"google_monitoring_uptime_check_config":                monitoring.MonitoringUptimeCheckConfigTfplan2caiConverter(),
 	"google_network_security_address_group":                networksecurity.NetworkSecurityAddressGroupTfplan2caiConverter(),
