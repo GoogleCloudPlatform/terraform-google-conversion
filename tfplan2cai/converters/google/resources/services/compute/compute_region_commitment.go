042f21b7a3271016e97c1fe91775b50045c6e7a3
The Magician <magic-modules@google.com>
2026-04-30 10:35:36 -0700

    Adds `params.resource_manager_tags` to `google_compute_region_commitm… (#17163) (#5556)
    [upstream:6f613bf2192c092a39900f9b00d59e6af89c7c72]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/compute/compute_region_commitment.go
+++ tfplan2cai/converters/google/resources/services/compute/compute_region_commitment.go
@@ -164,6 +164,12 @@ func GetComputeRegionCommitmentApiObject(d tpgresource.TerraformResourceData, co
 	} else if v, ok := d.GetOkExists("existing_reservations"); !tpgresource.IsEmptyValue(reflect.ValueOf(existingReservationsProp)) && (ok || !reflect.DeepEqual(v, existingReservationsProp)) {
 		obj["existingReservations"] = existingReservationsProp
 	}
+	paramsProp, err := expandComputeRegionCommitmentParams(d.Get("params"), d, config)
+	if err != nil {
+		return nil, err
+	} else if v, ok := d.GetOkExists("params"); !tpgresource.IsEmptyValue(reflect.ValueOf(paramsProp)) && (ok || !reflect.DeepEqual(v, paramsProp)) {
+		obj["params"] = paramsProp
+	}
 	regionProp, err := expandComputeRegionCommitmentRegion(d.Get("region"), d, config)
 	if err != nil {
 		return nil, err
@@ -299,6 +305,39 @@ func expandComputeRegionCommitmentAutoRenew(v interface{}, d tpgresource.Terrafo
 
 func expandComputeRegionCommitmentExistingReservations(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
 	return v, nil
+}
+
+func expandComputeRegionCommitmentParams(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
+	if v == nil {
+		return nil, nil
+	}
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+	raw := l[0]
+	original := raw.(map[string]interface{})
+	transformed := make(map[string]interface{})
+
+	transformedResourceManagerTags, err := expandComputeRegionCommitmentParamsResourceManagerTags(original["resource_manager_tags"], d, config)
+	if err != nil {
+		return nil, err
+	} else if val := reflect.ValueOf(transformedResourceManagerTags); val.IsValid() && !tpgresource.IsEmptyValue(val) {
+		transformed["resourceManagerTags"] = transformedResourceManagerTags
+	}
+
+	return transformed, nil
+}
+
+func expandComputeRegionCommitmentParamsResourceManagerTags(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (map[string]string, error) {
+	if v == nil {
+		return map[string]string{}, nil
+	}
+	m := make(map[string]string)
+	for k, val := range v.(map[string]interface{}) {
+		m[k] = val.(string)
+	}
+	return m, nil
 }
 
 func expandComputeRegionCommitmentRegion(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
