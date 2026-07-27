956239d8972f21424d9c83776b766a27dca99fbe
The Magician <magic-modules@google.com>
2026-07-27 13:35:50 -0700

    feat(workflows): make source_contents required for 8.0.0 (#18411) (#6050)
    [upstream:f4589ff7a01191a75bae921bbab65f1068bd3983]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- pkg/services/workflows/workflows_workflow.go
+++ pkg/services/workflows/workflows_workflow.go
@@ -77,6 +77,11 @@ const WorkflowsWorkflowSchemaName string = "google_workflows_workflow"
 func ResourceWorkflowsWorkflow() *schema.Resource {
 	return &schema.Resource{
 		Schema: map[string]*schema.Schema{
+			"source_contents": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: `Workflow code to be executed. The size limit is 128KB.`,
+			},
 			"call_log_level": {
 				Type:         schema.TypeString,
 				Optional:     true,
@@ -141,11 +146,6 @@ Using - as a wildcard for the {project} or not providing one at all will infer t
 The {account} value can be the email address or the unique_id of the service account.
 If not provided, workflow will use the project's default service account.
 Modifying this field for an existing workflow results in a new workflow revision.`,
-			},
-			"source_contents": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: `Workflow code to be executed. The size limit is 128KB.`,
 			},
 			"tags": {
 				Type:     schema.TypeMap,
