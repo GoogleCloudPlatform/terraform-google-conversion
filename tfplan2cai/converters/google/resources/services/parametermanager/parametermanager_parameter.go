1c1fe1e84e72766f7081c41f711bca14134251a7
The Magician <magic-modules@google.com>
2026-09-11 14:31:38 -0700

    Add tags to Parameter Manager Resource (#18942) (#6362)
    [upstream:b197960a80181c16eb5bd6a49d1f433b2bc71181]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/parametermanager/parametermanager_parameter.go
+++ tfplan2cai/converters/google/resources/services/parametermanager/parametermanager_parameter.go
@@ -122,6 +122,12 @@ func GetParameterManagerParameterApiObject(d tpgresource.TerraformResourceData, 
 	} else if v, ok := d.GetOkExists("kms_key"); !tpgresource.IsEmptyValue(reflect.ValueOf(kmsKeyProp)) && (ok || !reflect.DeepEqual(v, kmsKeyProp)) {
 		obj["kmsKey"] = kmsKeyProp
 	}
+	tagsProp, err := expandParameterManagerParameterTags(d.Get("tags"), d, config)
+	if err != nil {
+		return nil, err
+	} else if v, ok := d.GetOkExists("tags"); !tpgresource.IsEmptyValue(reflect.ValueOf(tagsProp)) && (ok || !reflect.DeepEqual(v, tagsProp)) {
+		obj["tags"] = tagsProp
+	}
 	effectiveLabelsProp, err := expandParameterManagerParameterEffectiveLabels(d.Get("effective_labels"), d, config)
 	if err != nil {
 		return nil, err
@@ -138,6 +144,17 @@ func expandParameterManagerParameterFormat(v interface{}, d tpgresource.Terrafor
 
 func expandParameterManagerParameterKmsKey(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
 	return v, nil
+}
+
+func expandParameterManagerParameterTags(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (map[string]string, error) {
+	if v == nil {
+		return map[string]string{}, nil
+	}
+	m := make(map[string]string)
+	for k, val := range v.(map[string]interface{}) {
+		m[k] = val.(string)
+	}
+	return m, nil
 }
 
 func expandParameterManagerParameterEffectiveLabels(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (map[string]string, error) {
